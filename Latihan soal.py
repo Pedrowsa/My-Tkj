c41bedbbdfc8d8eaa409be852fe6f771dec893e6
Valezar Pedrosa Susanto <117614899+Pedrowsa@users.noreply.github.com>
2022-11-08 08:09:40 +0700

    Add files via upload

---
+++ Latihan soal.py
@@ -0,0 +1,21 @@
+#soal 1#
+print ("Hello World To Python")
+
+#soal 2#
+print (10 + 5)
+print (10 - 5)
+print (10 * 5)
+print (10 // 5)
+
+#soal 3#
+varA = 10
+varB = 5
+
+print (type(10 // 5))
+print (type(varA // varB))
+print (type(10 / 5))
+
+#soal 4#
+usia = "16 tahun"
+kata = "usiaku:"
+print ("{} {}". format (kata,usia))
